dd9378e83eb74755eecc371147654cd334cfb203
davidoises <davidoises@hotmail.com>
2024-08-09 03:11:49 -0700

    Setting foundations for linux tests

---
+++ app/main_linux.c
@@ -0,0 +1,17 @@
+// #include "mcu.h"
+// #include "pio_driver_interface.h"
+
+// extern const struct pio_driver_interface pio_driver;
+
+#include <stdio.h>
+
+int main()
+{
+    // pio_driver.init();
+    printf("Start of program\n");
+
+    // while(1)
+    // {
+    // }
+    return 0;
+}
