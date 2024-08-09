691f85d4f223c80a134f2fc2de450dc739706389
davidoises <davidoises@hotmail.com>
2024-08-09 00:22:51 -0700

    Added UART configuration and incomplete base functions for reading and writing

--- app/main.c
+++ app/main.c
@@ -11,7 +11,10 @@ static void pio_clock_init(uint16_t *pio_pids, uint16_t pio_count);
 static void pio_init(void);
 
 static void usart_clock_init(void);
-static void usart_init(void);
+static void uart_init(void);
+static void uart_enable(void);
+static uint32_t usart_write(const uint8_t *buffer, const uint16_t length);
+static uint32_t usart_read(uint8_t *buffer, const uint16_t length);
 
 static void adc_clock_init(void);
 
@@ -23,16 +26,22 @@ uint16_t pio_pids[] = {ID_PIOB, ID_PIOC};
 int main()
 {
 
+    // General system initialization and main clocks
     system_init();
 
+    // Enable peripheral clocks
     pio_clock_init(pio_pids, sizeof(pio_pids)/sizeof(pio_pids[0]));
     usart_clock_init();
     adc_clock_init();
 
-    // usart_init();
+    // Configure peripherals
+    uart_init();
     // adc_init();
 
     pio_init();
+
+    // Start peripheral operation
+    uart_enable();
 
     while(1)
     {
@@ -42,6 +51,7 @@ int main()
 
         char output_msg[100];
 		uint16_t count = sprintf(output_msg, "The ADC measurement is: %d\r\n", adc_reding);
+        usart_write((uint8_t *)output_msg, count);
 
         // PORT->Group[0].OUT.reg &= ~PORT_PA27;
         // PORT->Group[1].OUT.reg &= ~PORT_PB03;
@@ -255,9 +265,72 @@ static void usart_clock_init(void)
     pmc_enable_peripheral_clock(ID_UART0);
 }
 
-static void usart_init(void)
+static void uart_init(void)
 {
+    // Set the write protection key and disable write protection
+    UART0->UART_WPMR = UART_WPMR_WPKEY_PASSWD;
 
+    // UART_CR_RSTRX - Put the receiver into reset state
+    // UART_CR_RSTTX - Put the transmiter into reset state
+    // UART_CR_RXDIS - disable receiver
+    // UART_CR_TXDIS - disable transmiter
+    UART0->UART_CR = (UART_CR_RSTRX | UART_CR_RXDIS | UART_CR_RSTTX | UART_CR_TXDIS);
+
+    // UART_CR_RSTSTA - Resets PARE, FRAME, CMP, OVRE in the UART_SR register
+    UART0->UART_CR = UART_CR_RSTSTA;
+
+    // UART_MR_BRSRCCK_PERIPH_CLK - Baud rate source clock is peripheral clock
+    // UART_MR_PAR_NO - no parity
+    // UART_MR_CHMODE_NORMAL - no testing mode
+    // UART_MR_FILTER_DISABLED - no filter
+    UART0->UART_MR = (UART_MR_BRSRCCK_PERIPH_CLK | UART_MR_PAR_NO | UART_MR_CHMODE_NORMAL | UART_MR_FILTER_DISABLED);
+
+    // Configure the UART baud rate
+    #define CONF_UART_0_BAUD 115200
+    #define CONF_UART0_FREQUENCY 150000000
+    #define CONF_UART_0_BAUD_CD ((CONF_UART0_FREQUENCY) / CONF_UART_0_BAUD / 16)
+    UART0->UART_BRGR = UART_BRGR_CD(CONF_UART_0_BAUD_CD);
+}
+
+static void uart_enable(void)
+{
+    // Enable the receiver and transmiter
+    UART0->UART_CR = (UART_CR_RXEN | UART_CR_TXEN);
+}
+
+static uint32_t usart_write(const uint8_t *buffer, const uint16_t length)
+{
+    // writing UART
+    // data transfered to UART_THR
+    // UART_SR.TXRDY indicates that the holding register is empty
+    // UART_SR.TXEMPTY indicates that the holding register and internal shift register are empty
+
+    uint32_t counter = 0;
+
+    while (counter < length)
+    {
+        // buffer[counter];
+        counter++;
+    }
+
+    return counter;
+}
+
+static uint32_t usart_read(uint8_t *buffer, const uint16_t length)
+{
+    // reading UART
+    // data transfered to UART_RHR
+    // UART_SR.RXRDY will indicate that the data is ready and it clears on its own after reading
+    // Not implemented, for now dummy structure
+
+    uint32_t counter = 0;
+    while (counter < length)
+    {
+        buffer[counter] = 'a' + counter;
+        counter++;
+    }
+
+    return counter;
 }
 
 static void adc_clock_init(void)
